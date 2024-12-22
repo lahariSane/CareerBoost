8b3d4cabb471c46f351200fd1022f9188a708614
VenkataSagar <mrsunday0708.23@gmail.com>
2024-12-22 20:56:35 +0530

    Created Landing-page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Layout/Navbar';
-import Footer from './Components/Layout/Footer';
+// import Navbar from './Components/Layout/Navbar';
+// import Footer from './Components/Layout/Footer';
 import LandingPage from './Components/LandingPage/LandingPage';
 
 function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        {/* Navbar */}
-        <Navbar />
+      
 
         {/* Main Content */}
         <main className="flex-grow">
@@ -18,8 +17,7 @@ function App() {
           </Routes>
         </main>
 
-        {/* Footer */}
-        <Footer />
+       
       </div>
     </Router>
   );
