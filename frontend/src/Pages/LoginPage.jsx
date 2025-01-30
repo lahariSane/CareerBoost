134788981b88e1daede1ea9b8e349ca0a6b4e19d
VenkataSagar <mrsunday0708.23@gmail.com>
2025-01-30 11:31:23 +0530

    Updated frontend

--- frontend/src/Pages/LoginPage.jsx
+++ frontend/src/Pages/LoginPage.jsx
@@ -6,6 +6,7 @@ import ForgotPassword from "../Components/Auth/ForgotPassword";
 import OTPVerification from "../Components/Auth/OTPVerification";
 import Illustration from "../Assests/undraw_engineering-team_13ax.svg";
 import ResetPassword from "../Components/Auth/ResetPassword";
+import careerboostlogo from '../Assests/careerboostlogo.png';
 
 const LoginPage = () => {
     const [authState, setAuthState] = useState("login"); // "login", "signup", "forgotPassword", "otpVerification", "resetPassword"
@@ -43,21 +44,21 @@ const LoginPage = () => {
             {/* Logo */}
             <div className="absolute top-4 left-4">
                 <img
-                    src="/path/to/logo.png"
+                     src={careerboostlogo}
                     alt="Logo"
                     className="h-10"
                 />
             </div>
-            <div className="relative w-[70%] flex items-center justify-center bg-primary-light rounded-2xl h-[650px]">
+            <div className="relative w-[70%] flex items-center justify-center bg-primary-light rounded-2xl h-[650px] shadow-2xl">
                 {/* Left Section */}
                 <div
-                    className={`w-full md:w-1/2 flex-1 flex flex-col justify-center items-center p-8 ${authState === "signup" ? "order-2 translate-x-10" : "order-1 translate-x-0"} transition-all duration-300 ease-in`}
+                    className={`w-full md:w-1/2 flex-1 flex flex-col justify-center items-center p-8 ${authState === "signup" ? "order-2 translate-x-0" : "order-1 translate-x-0"} transition-all duration-[300ms] ease-in-out`}
                 >
                     {/* Auth Form */}
-                    <div className="bg-primary-light shadow-md rounded-xl p-6 w-full max-w-md border-[1px] border-solid border-black">
+                    <div className="bg-primary-light shadow-[0px_10px_10px_rgba(0,0,0,0.25)] rounded-xl p-6 w-full max-w-md border-[2px] border-solid border-bg-primary-light">
                         <h2 className="text-2xl font-bold text-center mb-4">
-                            {authState === "login" && "Log In"}
-                            {authState === "signup" && "Sign Up"}
+                            {authState === "login" && "LOG IN"}
+                            {authState === "signup" && "SIGN UP"}
                             {authState === "forgotPassword" && "Forgot Password"}
                             {authState === "otpVerification" && "OTP Verification"}
                             {authState === "resetPassword" && "Reset Password"}
