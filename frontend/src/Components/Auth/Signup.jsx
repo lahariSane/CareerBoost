26149a2c2c2f188b86b72c20d96d11dd5d693a49
Lahari <laharisane1511@gmail.com>
2025-03-05 05:43:45 +0530

    Refactor Signup component: update GitHub signup button styling and adjust OTP request logic

--- frontend/src/Components/Auth/Signup.jsx
+++ frontend/src/Components/Auth/Signup.jsx
@@ -19,12 +19,6 @@ const Signup = ({ onSignupComplete }) => {
     setError(null); // Reset error state
 
     try {
-      // Make the POST request for signing up
-      // const response = await axios.post(`${process.env.REACT_APP_API}/auth/register`, {
-      //   name,
-      //   email,
-      //   password,
-      // })
       const response = await axios.post(`${process.env.REACT_APP_API}/auth/sendotp`, {
         name,
         email,
@@ -136,13 +130,15 @@ const Signup = ({ onSignupComplete }) => {
       <div className="w-full flex flex-col justify-between items-center mt-4">
 
         <GoogleLogin onSuccess={handleGoogleSignup} onFailure={(error) => setError(error)} />
-
         <button
           type="button"
+          className="mt-2 py-2 px-2 bg-white text-black flex items-center justify-center"
           onClick={handleGitHubSignup}
-          className="w-3/4 mt-2 py-2 px-4 bg-secondary-mauve text-white rounded-md hover:bg-secondary-green"
         >
-          Sign up with GitHub
+          <svg className="w-5 h-5 mr-2" viewBox="0 0 24 24" fill="currentColor">
+            <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.207 11.387.6.113.793-.263.793-.587 0-.287-.012-1.243-.012-2.263-3.338.75-4.042-1.612-4.042-1.612-.544-1.387-1.331-1.75-1.331-1.75-1.087-.75.087-.75.087-.75 1.2.087 1.837 1.2 1.837 1.2 1.087 1.837 2.837 1.312 3.537 1 .113-.75.425-1.312.75-1.612-2.662-.3-5.462-1.337-5.462-5.925 0-1.312.462-2.4 1.2-3.262-.113-.3-.525-1.512.113-3.112 0 0 1.012-.3 3.3 1.2.975-.263 2.025-.375 3.075-.375 1.05 0 2.1.112 3.075.375 2.287-1.5 3.3-1.2 3.3-1.2.637 1.6.225 2.812.113 3.112.75.862 1.2 1.95 1.2 3.262 0 4.6-2.8 5.625-5.475 5.925.45.375.825 1.087.825 2.2 0 1.612-.012 2.912-.012 3.3 0 .337.188.712.8.587C20.562 21.8 24 17.3 24 12c0-6.63-5.37-12-12-12z" />
+          </svg>
+          Sign in with GitHub
         </button>
       </div>
     </form>
